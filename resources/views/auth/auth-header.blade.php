6f0cc3bdb4831a6797843d02973add3603540f07
Sowren Sen <sowrensen@gmail.com>
2020-11-30 20:18:37 +0600

    Load image on auth views only when specified in config

--- resources/views/auth/auth-header.blade.php
+++ resources/views/auth/auth-header.blade.php
@@ -1,9 +1,11 @@
 <h1 class="uk-text-center uk-margin-small-bottom {{ config('uikit.auth.heading_classes') }}">
     <span>
-        <img src="{{ asset(config('uikit.brand_logo')) }}"
-             class="{{ config('uikit.auth.heading_image_classes') }}"
-             style="max-width: 90px; max-height: 90px"
-             alt="{{ config('uikit.navbar.logo.image_alt_text') }}">
+        @if(config('uikit.brand_logo', null))
+            <img src="{{ asset(config('uikit.brand_logo')) }}"
+                 class="{{ config('uikit.auth.heading_image_classes') }}"
+                 style="max-width: 90px; max-height: 90px"
+                 alt="{{ config('uikit.navbar.logo.image_alt_text') }}">
+        @endif
         {{ config('uikit.brand_name') }}
     </span>
 </h1>
