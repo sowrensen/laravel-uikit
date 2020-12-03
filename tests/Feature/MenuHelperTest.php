536f8fad573b90bfa6e06c92d12a783efccf0801
Sowren Sen <sowrensen@gmail.com>
2020-12-03 17:39:34 +0600

    Tests for menu helper

---
+++ tests/Feature/MenuHelperTest.php
@@ -0,0 +1,98 @@
+<?php
+
+
+namespace Sowren\LaravelUikit\Test\Feature;
+
+use Sowren\LaravelUikit\Test\TestCase;
+use Sowren\LaravelUikit\Helpers\MenuHelper;
+
+class MenuHelperTest extends TestCase
+{
+    /** @test */
+    public function testIsHeader()
+    {
+        $item0 = ['header' => 'General'];
+        $item1 = 'General';
+        $this->assertTrue(MenuHelper::isHeader($item0));
+        $this->assertTrue(MenuHelper::isHeader($item1));
+    }
+
+    /** @test */
+    public function testIDivider()
+    {
+        $item0 = ['divider' => true];
+        $item1 = ['divider' => false];
+        $this->assertTrue(MenuHelper::isDivider($item0));
+        $this->assertFalse(MenuHelper::isDivider($item1));
+    }
+
+    /** @test */
+    public function testIsLinkWithUrl()
+    {
+        $item = [
+            'text' => 'Profile',
+            'url' => '#'
+        ];
+        $this->assertTrue(MenuHelper::isLink($item));
+    }
+
+    /** @test */
+    public function testIsLinkWithRoute()
+    {
+        $item = [
+            'text' => 'Profile',
+            'route' => 'profile'
+        ];
+        $this->assertTrue(MenuHelper::isLink($item));
+    }
+
+    /** @test */
+    public function isSubmenu()
+    {
+        $item = [
+            'text' => 'Accounts',
+            'submenu' => [
+                [
+                    'text' => 'Profile',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'Change Avatar',
+                    'url' => '#',
+                ],
+            ]
+        ];
+        $this->assertTrue(MenuHelper::isSubmenu($item));
+    }
+
+    /** @test */
+    public function testItemAllowed()
+    {
+        $item = [
+            'text' => 'Profile',
+            'url' => '#',
+            'restricted' => false
+        ];
+        $this->assertTrue(MenuHelper::isAllowed($item));
+    }
+
+    /** @test */
+    public function testItemNotAllowed()
+    {
+        $item = [
+            'text' => 'Profile',
+            'url' => '#',
+            'restricted' => true
+        ];
+        $this->assertFalse(MenuHelper::isAllowed($item));
+    }
+
+    /** @test */
+    public function testIsValidSidebarItem()
+    {
+        $item = [
+            'strange_key' => ['with strange value']
+        ];
+        $this->assertFalse(MenuHelper::isValidSidebarItem($item));
+    }
+}
