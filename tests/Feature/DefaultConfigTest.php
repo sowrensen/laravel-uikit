db988fba18454adbe544e2a926ffd6d7b3ee7109
Sowren Sen <sowrensen@gmail.com>
2020-12-03 17:24:13 +0600

    Tests for conifg, event, and listener

--- tests/Feature/DefaultConfigTest.php
+++ tests/Feature/DefaultConfigTest.php
@@ -3,8 +3,31 @@
 
 namespace Sowren\LaravelUikit\Test\Feature;
 
+use Sowren\LaravelUikit\Test\TestCase;
+use Sowren\LaravelUikit\Menu\Filters\HrefFilter;
+use Sowren\LaravelUikit\Menu\Filters\ClassFilter;
+use Sowren\LaravelUikit\Menu\Filters\ActiveFilter;
 
-class DefaultConfigTest
+class DefaultConfigTest extends TestCase
 {
+    /** @test */
+    public function testUiConfig()
+    {
+        $this->assertNull(\Config::get('uikit.brand_logo'));
+        $this->assertEquals('#262626', \Config::get('uikit.navbar.background'));
+        $this->assertFalse(\Config::get('uikit.navbar.logo.enabled'));
+        $this->assertTrue(\Config::get('uikit.navbar.user_section.enabled'));
+        $this->assertIsArray(\Config::get('uikit.sidebar.menu'));
+        $this->assertEquals('#EAEAEA', \Config::get('uikit.footer.background'));
+    }
 
+    /** @test */
+    public function testFilters()
+    {
+        $filters = \Config::get('uikit.filters');
+        $this->assertNotEmpty($filters);
+        $this->assertEquals(HrefFilter::class, $filters[0]);
+        $this->assertEquals(ActiveFilter::class, $filters[1]);
+        $this->assertEquals(ClassFilter::class, $filters[2]);
+    }
 }
--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -3,8 +3,31 @@
 
 namespace Sowren\LaravelUikit\Test\Feature;
 
+use Sowren\LaravelUikit\Uikit;
+use Sowren\LaravelUikit\Test\TestCase;
+use Sowren\LaravelUikit\Events\MenuCompiling;
 
-class EventTest
+class EventTest extends TestCase
 {
+    /** @test */
+    public function testEventIsFired()
+    {
+        \Event::fake([
+            MenuCompiling::class
+        ]);
+        $uikit = $this->app->make(Uikit::class);
+        $this->assertIsArray($uikit->menu());
+        \Event::assertDispatched(MenuCompiling::class);
+    }
 
+    /** @test */
+    public function testListenerIsWorking()
+    {
+        $uikit = $this->app->make(Uikit::class);
+        $menu = $uikit->menu();
+        $this->assertNotEmpty($menu);
+        $this->assertArrayHasKey('href', $menu[1]);
+        $this->assertArrayHasKey('active', $menu[1]);
+        $this->assertArrayHasKey('class', $menu[1]);
+    }
 }
