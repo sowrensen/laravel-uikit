1890d97fe3dfa118b1e8cb8a99e804d88bc04246
Sowren Sen <sowrensen@gmail.com>
2021-03-15 16:06:04 +0600

    add section for session message flashing in auth pages

--- resources/views/auth/auth-main.blade.php
+++ resources/views/auth/auth-main.blade.php
@@ -8,7 +8,15 @@
 @section('body')
     <div class="uk-flex uk-height-viewport uk-flex-center uk-flex-middle">
         <div class="uk-width-1-3@xl uk-width-1-2@m">
+            @if(session('status'))
+                <div class="uk-alert-success" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <p>{{ session('status') }}</p>
+                </div>
+            @endif
+
             @include('uikit::auth.auth-heading')
+
             <div class="uk-card {{ config('uikit.auth.card_classes') }}">
                 @yield('auth_content')
             </div>
