1806df6fd4d9aa511685c3580c5847a885cfae62
Sowren Sen <sowrensen@gmail.com>
2021-03-15 17:14:56 +0600

    wip

--- resources/views/auth/auth-main.blade.php
+++ resources/views/auth/auth-main.blade.php
@@ -8,11 +8,20 @@
 @section('body')
     <div class="uk-flex uk-height-viewport uk-flex-center uk-flex-middle">
         <div class="uk-width-1-3@xl uk-width-1-2@m">
+            {{-- Flash session message --}}
             @if(session('status'))
-                <div class="uk-alert-success" uk-alert>
-                    <a class="uk-alert-close" uk-close></a>
-                    <p>{{ session('status') }}</p>
-                </div>
+                @if(session('status') == 'verification-link-sent')
+                    <div class="uk-alert-success" uk-alert>
+                        <a class="uk-alert-close" uk-close></a>
+                        <p> A new verification link has been sent to the email address you provided during
+                            registration.</p>
+                    </div>
+                @else
+                    <div class="uk-alert-success" uk-alert>
+                        <a class="uk-alert-close" uk-close></a>
+                        <p>{{ session('status') }}</p>
+                    </div>
+                @endif
             @endif
 
             @include('uikit::auth.auth-heading')
